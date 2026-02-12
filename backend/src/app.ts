22b9aa7c3cd1cbac833d032c51a31a31a9e92f4e
MelvinVerLia <verdinanmelvin@gmail.com>
2026-02-12 20:26:50 +0700

    feat: refactor server and app setup to integrate socket.io and improve structure

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,22 +12,11 @@ import cookieParser from "cookie-parser";
 import passport from "./config/GoogleStrategy";
 import dotenv from "dotenv";
 
-import { createServer } from "http";
-import { Server } from "socket.io";
-import { RegisterSocket } from "./utils/RegisterSocket";
-
 dotenv.config();
 
 const app = express();
-const httpServer = createServer(app);
 
 const origins = process.env.FRONTEND_URL_PROD || process.env.FRONTEND_URL;
-const io = new Server(httpServer, {
-  cors: {
-    origin: origins,
-    credentials: true,
-  },
-});
 app.use(
   cors({
     origin: origins,
@@ -47,12 +36,6 @@ app.use("/api/chats", chatRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-});
-
-RegisterSocket(io);
-
-httpServer.listen(process.env.SOCKET_PORT, () => {
-  console.log(`Socket Server running on port ${process.env.SOCKET_PORT}`);
 });
 
 export default app;
